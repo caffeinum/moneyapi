17d1bc0c4bc6c47bc8e91a74cfb8555a6aec0f6b
Aleksey Bykhun <theisease@gmail.com>
2017-08-28 01:28:27 +0300

    Cleaned code a bit

--- src/main/scala/com/caffeinum/moneyapi/InMemoryRepository.scala
+++ src/main/scala/com/caffeinum/moneyapi/InMemoryRepository.scala
@@ -2,10 +2,8 @@ package com.caffeinum.moneyapi
 
 import java.util.concurrent.atomic.AtomicLong
 import scala.collection.concurrent.TrieMap
-import io.circe._
 
 class StorageException(message: String) extends Exception(message) {
-  def toJson = Json.obj("error" -> Json.fromString(message))
   override def toString = message
 }
 
@@ -85,56 +83,3 @@ class Storage {
     user.balance
   }
 }
-
-
-
-
-
-//
-//
-//
-// class InMemoryRepository extends MoneyRepository {
-//
-//   private val users = TrieMap[Long, User]()
-//   private val idSequence = new AtomicLong()
-//
-//   def getUsers: Seq[User] = users.values.toSeq
-//   def getUserById(id: Int): Option[User] = ???
-//   def create(user: User): User = ???
-//   def delete(id: Int): Unit = ???
-//   //def update(id: Int, userForm: UserForm): Option[User]
-//
-//   def create(transfer: MoneyTransfer): MoneyTransfer = ???
-//   //def update(id: Int, transfer:): MoneyTransfer
-//   def getTransferById(id: Int): MoneyTransfer = ???
-//   def getTransfersByUserId(id: Int): Seq[MoneyTransfer] = ???
-  //
-
-  //
-  // override def getAll: Seq[TodoItem] =
-  //   repository.values.toSeq
-  //
-  // override def getById(id: Long): Option[TodoItem] =
-  //   repository.get(id)
-  //
-  // override def create(todoItemForm: TodoItemPostForm): TodoItem = {
-  //   val id       = idSequence.getAndIncrement
-  //   val todoItem = TodoItem(id, todoItemForm)
-  //   repository.put(id, todoItem)
-  //   todoItem
-  // }
-  //
-  // override def delete(id: Long): Unit =
-  //   repository.remove(id)
-  //
-  // override def deleteAll: Unit =
-  //   synchronized {
-  //     idSequence.set(0)
-  //     repository.clear()
-  //   }
-  //
-  // override def update(id: Long, todoItemForm: TodoItemPatchForm): Option[TodoItem] = {
-  //   val item = repository.get(id).map(_.update(todoItemForm))
-  //   item.foreach(repository.update(id, _))
-  //   item
-  // }
--- src/main/scala/com/caffeinum/moneyapi/User.scala
+++ src/main/scala/com/caffeinum/moneyapi/User.scala
@@ -1,7 +1,4 @@
 package com.caffeinum.moneyapi
 
-import java.util.UUID
-
 case class User(val id: Long, val username: String, val balance: Long = 0)
-
 case class MoneyTransfer(val senderID: Long, val recipientID: Long, val amount: Long)
