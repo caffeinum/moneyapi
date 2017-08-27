f88deb20ae901e595c9b503d1daeb8cfd3d1e879
Aleksey Bykhun <theisease@gmail.com>
2017-08-28 02:43:26 +0300

    bash curl testing script

--- tests.sh
+++ tests.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 HOST="http://localhost:8080"
-
+U=0
 echo "Testing user creation at $HOST/users"
 
 echo "Get user who is not existant"
@@ -13,47 +14,46 @@ echo "Testing another user wth the same name"
 curl -X POST $HOST/users -d "username=root" && echo
 
 echo "Deposit user root twice by 100"
-curl -X PUT $HOST/users/1/deposit -d "amount=100" && echo
-curl -X PUT $HOST/users/1/deposit -d "amount=100" && echo
+curl -X PUT $HOST/users/$U/deposit -d "amount=100" && echo
+curl -X PUT $HOST/users/$U/deposit -d "amount=100" && echo
 
 echo "Send money from root to admin amount 150"
-curl -X PUT $HOST/users/1/send/0 -d "amount=150" && echo
+curl -X PUT $HOST/users/$U/send/0 -d "amount=150" && echo
 
 echo "Send money from root to admin amount 100 but he does not have that much"
-curl -X PUT $HOST/users/1/send/0 -d "amount=100" && echo
+curl -X PUT $HOST/users/$U/send/0 -d "amount=100" && echo
 
 echo "He tries to withdraw 100 but fails too"
-curl -X PUT $HOST/users/1/withdraw -d "amount=100" && echo
+curl -X PUT $HOST/users/$U/withdraw -d "amount=100" && echo
 
 echo "He tries to withdraw 50 and succeeds"
-curl -X PUT $HOST/users/1/withdraw -d "amount=50" && echo
-
+curl -X PUT $HOST/users/$U/withdraw -d "amount=50" && echo
 
 
 U1=1
 U2=2
 echo "Deal with concurrency"
-COUNT=5
+echo "Deposit $U1 with 1000"
+curl -X PUT $HOST/users/$U1/deposit -d "amount=1000" && echo
+echo "At the beginning, balances are:"
+curl $HOST/users/$U1 && echo
+curl $HOST/users/$U2 && echo
+COUNT=100
 echo "We try $COUNT times to send back and forth money and see if the system fails to maintain zero-sum"
-for i in 1..$COUNT
-do	
-	amount=($RANDOM % 100)
+while [ $COUNT -gt 0 ]; do
+	amount=$((RANDOM%100))
 	echo "$U1 -> $U2: send $amount"
-	curl -X PUT $HOST/users/$U1/send/$U2 -d "amount=$amount" &
-	
-	amount=($RANDOM % 100)
+	curl -X PUT $HOST/users/$U1/send/$U2 -d "amount=$amount" && echo
+
+	amount=$((RANDOM%100))
 	echo "$U2 -> $U1: send $amount"
-	curl -X PUT $HOST/users/$U2/send/$U1 -d "amount=$amount" &
+	curl -X PUT $HOST/users/$U2/send/$U1 -d "amount=$amount" && echo
+
+	let COUNT=COUNT-1
 done
 
 wait
 
 echo "At the end, balances are:"
-curl $HOST/users/0 && echo
-curl $HOST/users/1 && echo
-
-
-
-
-
-
+curl $HOST/users/$U1 && echo
+curl $HOST/users/$U2 && echo
